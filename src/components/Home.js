07b01647e82beb95670713494e8d52f3ce944740
Sam Alty <sam_alty@outlook.com>
2020-10-23 13:31:11 +0100

    Mapped through home page data and removed hard coding.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,14 @@
-import React, { Component, useEffect } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 import './Home.scss';
 import HomeNavbar from './HomeNavbar';
 import CaseStudies from './CaseStudies';
+import { HomeData } from './HomeData';
 import Footer from './Footer';
 import logo from '../media/logo.png';
 import home from '../media/home.jpg';
-import about from '../media/about.jpg';
-import opportunities from '../media/opportunities.jpg';
-import claims from '../media/claims.jpg';
 
 class Home extends Component {
     constructor(props) {
@@ -34,109 +32,30 @@ class Home extends Component {
                     <br></br>
                     <button className="btn">BOOK A FREE CONSULTATION</button>
                 </div>
-                <div className="intro-background">
-                    <div className="link-container" data-aos="fade-up">
-                        <div className="text-box grey top-left">
-                            <h2>Preparing your platform for innovation</h2>
-                            <hr></hr>
-                            <h3>At Gov Tax Incentive Consultants, we provide expert advice to pioneering UK businesses, 
-                                helping them to compliantly capitalise on available government grants and incentives.</h3>
-                            <Link to="/about" className="intro-link">
-                                <button className="intro-btn"><span>About us </span></button>
-                            </Link>
-                        </div>
-                        <div className="decoration white"></div>
-                        <div className="image-container bottom-right">
-                            <img src={about} alt="About us" className="intro-img"></img>
-                        </div>
-                        <Link to="/about" className="intro-link-mobile">
-                            <button className="intro-btn-mobile"><span>About us </span></button>
-                        </Link>
-                    </div>
-                </div>
-                <div className="intro-background reverse">
-                    <div className="link-container" data-aos="fade-up">
-                        <div className="text-box white top-right">
-                            <h2>Never miss an opportunity with GTIC</h2>
-                            <hr></hr>
-                            <h3>Our goal is to reward companies of all sectors, sizes and structures for the pioneering 
-                                work that they undertake. Achieving this requires specialist knowledge of a broad range 
-                                of incentives that few other consultancies possess.</h3>
-                            <Link to="/opportunities" className="intro-link">
-                                <button className="intro-btn float-right white"><span>Opportunities </span></button>
-                            </Link>
-                        </div>
-                        <div className="decoration grey"></div>
-                        <div className="image-container bottom-left">
-                            <img src={opportunities} alt="Opportunities" className="intro-img"></img>
-                        </div>
-                        <Link to="/opportunities" className="intro-link-mobile">
-                            <button className="intro-btn-mobile"><span>Opportunities </span></button>
-                        </Link>
-                    </div>
-                </div>
-                <div className="intro-background">
-                    <div className="link-container" data-aos="fade-up">
-                        <div className="text-box grey top-left">
-                            <h2>Making a claim with GTIC</h2>
-                            <hr></hr>
-                            <h3>We promise maximum value and total transparency. Our rigorous claims process blueprint 
-                                is designed to identify and capitalise on all available opportunities, while keeping 
-                                you in the loop every step of the way.</h3>
-                            <Link to="/making_a_claim" className="intro-link">
-                                <button className="intro-btn"><span>Making a claim </span></button>
-                            </Link>
-                        </div>
-                        <div className="decoration white"></div>
-                        <div className="image-container bottom-right">
-                            <img src={claims} alt="Making a claim" className="intro-img"></img>
-                        </div>
-                        <Link to="/making_a_claim" className="intro-link-mobile">
-                            <button className="intro-btn-mobile"><span>Making a claim </span></button>
-                        </Link>
-                    </div>
-                </div>
-                <div className="intro-background reverse">
-                    <div className="link-container" data-aos="fade-up">
-                        <div className="text-box white top-right">
-                            <h2>R&D Tax Relief explained</h2>
-                            <hr></hr>
-                            <h3>A generous initiative rewarding innovation within UK industry, Research & Development 
-                                (R&D) Tax Relief could provide your business with a competitive advantage and firm 
-                                foundations for future growth.</h3>
-                            <Link to="/" className="intro-link">
-                                <button className="intro-btn float-right white"><span>How it works </span></button>
-                            </Link>
-                        </div>
-                        <div className="decoration grey"></div>
-                        <div className="image-container bottom-left">
-                            <img src={opportunities} alt="Opportunities" className="intro-img"></img>
-                        </div>
-                        <Link to="/" className="intro-link-mobile">
-                            <button className="intro-btn-mobile"><span>How it works </span></button>
-                        </Link>
-                    </div>
-                </div>
-                <div className="intro-background">
-                    <div className="link-container" data-aos="fade-up">
-                        <div className="text-box grey top-left">
-                            <h2>Insights</h2>
-                            <hr></hr>
-                            <h3>Insights is where we share much of our expertise directly with you. If you have a 
-                                query relating to a particular tax incentive or grant, here is where we will  
-                                hopefully have you covered.</h3>
-                            <Link to="/" className="intro-link">
-                                <button className="intro-btn"><span>Insights </span></button>
-                            </Link>
-                        </div>
-                        <div className="decoration white"></div>
-                        <div className="image-container bottom-right">
-                            <img src={claims} alt="Making a claim" className="intro-img"></img>
-                        </div>
-                        <Link to="/" className="intro-link-mobile">
-                            <button className="intro-btn-mobile"><span>Insights </span></button>
-                        </Link>
-                    </div>
+                <div className="map-container">
+                    {HomeData.map((item, index) => {
+                        return (
+                            <div className="intro-background" key={index}>
+                                <div className="link-container" data-aos="fade-up">
+                                    <div className="text-box">
+                                        <h2>{item.title}</h2>
+                                        <hr></hr>
+                                        <h3>{item.description}</h3>
+                                        <Link to={item.path} className="intro-link">
+                                            <button className="intro-btn"><span>{item.buttonText}</span></button>
+                                        </Link>
+                                    </div>
+                                    <div className="decoration white"></div>
+                                    <div className="image-container bottom-right">
+                                        <img src={item.img} alt={item.buttonText} className="intro-img"></img>
+                                    </div>
+                                    <Link to={item.path} className="intro-link-mobile">
+                                        <button className="intro-btn-mobile"><span>{item.buttonText}</span></button>
+                                    </Link>
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
                 <div className="case-studies">
                     <div className="link-container" data-aos="fade-up">
---
+++ src/components/HomeData.js
@@ -0,0 +1,48 @@
+import about from '../media/about.jpg';
+import opportunities from '../media/opportunities.jpg';
+import claims from '../media/claims.jpg';
+
+export const HomeData = [
+    {
+        title: 'Preparing your platform for innovation',
+        description:`At Gov Tax Incentive Consultants, we provide expert advice to pioneering UK businesses, helping them to 
+                     compliantly capitalise on available government grants and incentives.`,
+        path: '/about',
+        buttonText: 'About us ',
+        img: about
+    },
+    {
+        title: 'Never miss an opportunity with GTIC',
+        description:`Our goal is to reward companies of all sectors, sizes and structures for the pioneering work that they 
+                     undertake. Achieving this requires specialist knowledge of a broad range of incentives that few other 
+                     consultancies possess.`,
+        path: '/opportunities',
+        buttonText: 'Opportunities ',
+        img: opportunities
+    },
+    {
+        title: 'Making a claim with GTIC',
+        description:`We promise maximum value and total transparency. Our rigorous claims process blueprint is designed to 
+                     identify and capitalise on all available opportunities, while keeping you in the loop every step of the 
+                     way.`,
+        path: '/making_a_claim',
+        buttonText: 'Making a claim ',
+        img: claims
+    },
+    {
+        title: 'R&D Tax Relief explained',
+        description:`A generous initiative rewarding innovation within UK industry, Research & Development (R&D) Tax Relief 
+                     could provide your business with a competitive advantage and firm foundations for future growth.`,
+        path: '/',
+        buttonText: 'How it works ',
+        img: opportunities
+    },
+    {
+        title: 'Insights',
+        description:`Insights is where we share much of our expertise directly with you. If you have a query relating to a 
+                     particular tax incentive or grant, here is where we will hopefully have you covered.`,
+        path: '/',
+        buttonText: 'Insights ',
+        img: claims
+    }
+];
