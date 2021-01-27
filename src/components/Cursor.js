dffa9a721c5e488593a99231f09156fc34205b9b
Sam Alty <sam_alty@outlook.com>
2021-01-27 11:23:28 +0000

    Fixed cursor and contact page footer issues.

--- src/components/Cursor.js
+++ src/components/Cursor.js
@@ -4,12 +4,16 @@ import './styles/Cursor.scss';
 const Cursor = () => {
     const cursorRef = React.useRef(null);
     React.useEffect(() => {
-        document.addEventListener('mousemove', (event) => {
+        const onMouseMove = (event) => {
             const { clientX, clientY } = event;
             const mouseX = clientX - cursorRef.current.clientWidth / 2;
             const mouseY = clientY - cursorRef.current.clientHeight / 2;
             cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-        });
+        }
+        document.addEventListener('mousemove', onMouseMove);
+        return () => {
+            document.removeEventListener('mousemove', onMouseMove);
+        }
     }, [])
     return <div className="cursor" ref={cursorRef} />;
 };
